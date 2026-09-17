4bfd70280d2673b914a8fc7f10a31f10582f1373
BkLions228 <kellanw85@gmail.com>
2026-09-17 19:30:32 -0400

    feat: add secure GitHub Actions frontend deployment

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -10,4 +10,22 @@ module "static_site" {
   cloudfront_comment = (
     "${var.project_name} ${var.environment} static portfolio"
   )
+
+
+}
+
+module "github_oidc" {
+  source = "../../modules/github-oidc"
+
+  github_organization = var.github_organization
+  github_repository   = var.github_repository
+  github_branch       = var.github_deployment_branch
+
+  role_name = "${var.project_name}-${var.environment}-github-deploy"
+
+  site_bucket_name = module.static_site.site_bucket_name
+
+  cloudfront_distribution_arn = (
+    module.static_site.cloudfront_distribution_arn
+  )
 }
